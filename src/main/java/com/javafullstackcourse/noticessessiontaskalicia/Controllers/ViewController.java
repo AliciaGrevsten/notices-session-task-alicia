8a0fc207c385aa6af176c1a9e29dd78b99e28d43
AliciaGrevsten <alicia.grevsten@gmail.com>
2020-08-14 08:16:44 +0200

    Saving because computer is acting up. Started on login page.

--- src/main/java/com/javafullstackcourse/noticessessiontaskalicia/Controllers/ViewController.java
+++ src/main/java/com/javafullstackcourse/noticessessiontaskalicia/Controllers/ViewController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class ActionController {
+public class ViewController {
 
     @Autowired
     private NoticeService noticeService;
